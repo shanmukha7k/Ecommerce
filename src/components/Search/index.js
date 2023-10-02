632367edf466dab1c0dd72ae100ac738dda722fc
shanmukha7k <shanmukhk2000@gmail.com>
2023-10-02 20:07:15 +0530

    first commit

---
+++ src/components/Search/index.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { SearchContext } from '../../Context/SearchContext';
+import './index.css';
+
+const Search = () => {
+    const search = useContext(SearchContext);
+    const [, setSearchParam] = useSearchParams(); // Removed 'searchParam' since it's not used
+
+    useEffect(() => {
+        const searchQuery = {
+            query: search.searchQuery
+        };
+        
+        setSearchParam(searchQuery, { replace: true });
+    }, [search.searchQuery, setSearchParam]);
+
+    return ( 
+        <div className="search__container">
+            <div className="search__container__header">
+                <h1>No results found for "{search.searchQuery}"</h1>
+            </div>
+        </div>
+    );
+}
+ 
+export default Search;
